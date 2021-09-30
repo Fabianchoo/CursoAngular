072b1478deebc375977c93e3c94738123eb902a8
Fabian Rioseco Alcantara <fabian.rioseco@hotmail.com>
2021-09-30 16:30:47 -0300

    a

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -10,4 +10,11 @@ export class AppComponent {
   age = 18;
   img = 'https://media.discordapp.net/attachments/516433396596211712/893168319966896168/ExT9Z8kWQAEHiak.png';
   btnDisabled = true;
+
+  //Objeto para realizar la Property Binding
+  persona = {
+    name: 'Fabian',
+    age: 18,
+    avatar: 'https://media.discordapp.net/attachments/516433396596211712/893168319966896168/ExT9Z8kWQAEHiak.png'
+  }
 }
